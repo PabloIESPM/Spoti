cee040ea02d2f66d706958a0eff55c36e3acaaff
PabloIESPM <119699039+PabloIESPM@users.noreply.github.com>
2025-05-25 21:00:02 +0200

    Update VideogameController.php

--- src/app/Controladores/VideogameController.php
+++ src/app/Controladores/VideogameController.php
@@ -7,7 +7,7 @@ use App\Modelos\VideogameModel;
 class VideogameController
 {
     public function inicializar(){
-        //Hacer una petición a la API de videojuegos y obtener 5 videojuegos
+        //Hacer una petición a la API de videojuegos y obtener 5 videojuegos (POST: https://id.twitch.tv/oauth2/token?client_id=abcdefg12345&client_secret=hijklmn67890&grant_type=client_credentials)
 
         VideogameModel::inicializar($videjuegos);
 
@@ -17,7 +17,7 @@ class VideogameController
     public function index($id){
         //Haces una petición al modelo para obtener los datos del videojuego
 
-        //Incluyes la vista que muestre los datos del videojuego
+        //Incluyes la vista que muestre los datos del videojuego (videojuegos.php)
 
     }
 }
